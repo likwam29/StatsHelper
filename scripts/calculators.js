ace7b269844d23346a918eaed7fe09f1411b70ab
likwam29 <deftblow34@gmail.com>
2017-01-04 23:06:12 -0600

    final push of the night
    pushing up the most recent code.

--- scripts/calculators.js
+++ scripts/calculators.js
@@ -1,3 +1,9 @@
 function calculateZInterval(){
 	// do calculations in here.
+	var button = document.createElement('button');
+	var textNode = document.createTextNode('Click Me!');
+	button.appendChild(textNode);
+	button.className = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect';
+	componentHandler.upgradeElement(button);
+	document.getElementById('container').appendChild(button);
 }
